f88746fa64932e8ec06a98c615a611d41e4b676f
Roberto O. Fernández Crisial <rofc@rofc.com.ar>
2015-08-03 11:55:40 -0300

    thumbor.conf changes

--- thumbor.sh
+++ thumbor.sh
@@ -3,6 +3,5 @@
 sed -i "s/#SECURITY_KEY = 'MY_SECURE_KEY'/SECURITY_KEY = '$TB_SECURITY_KEY'/g" /etc/thumbor.conf
 sed -i "s/#ALLOW_UNSAFE_URL = True/ALLOW_UNSAFE_URL = $TB_ALLOW_UNSAFE_URL/g" /etc/thumbor.conf
 sed -i "s/#STORAGE_EXPIRATION_SECONDS = 2592000/STORAGE_EXPIRATION_SECONDS = $TB_STORAGE_EXPIRATION_SECONDS/g" /etc/thumbor.conf
-sed -i "s/#FILE_STORAGE_ROOT_PATH = '\/tmp\/thumbor\/storage'/FILE_STORAGE_ROOT_PATH = '\/tmp\/thumbor\/storage'/g" /etc/thumbor.conf
 
 exec thumbor
